237ee58d42b16b4e193940113144756f7b98d5de
Elvin46 <elvinva@code.edu.az>
2023-09-13 17:36:06 +0400

    Finish

---
+++ index.js
@@ -0,0 +1,63 @@
+// STAR RATING
+/////////////////////////////
+var stars = document.querySelectorAll('.stars a');
+stars.forEach((item, index1) => {
+    item.addEventListener('click', () => {
+        stars.forEach((star, index2) => {
+            index1 >= index2 ? star.classList.add('active') : star.classList.remove('active')
+        })
+    })
+})
+/////////////////////////////
+
+// MENU
+var btn_about = document.querySelector("#AboutBtn");
+var btn_promotion = document.querySelector("#PromotionBtn");
+var btn_before_after = document.querySelector("#Before-AfterBtn");
+var btn_comment = document.querySelector("#CommentBtn");
+var about = document.querySelector("#About");
+var promotion = document.querySelector("#Promotion");
+var before_after = document.querySelector("#BeforeAfter");
+var comment = document.querySelector("#Comment");
+
+
+btn_about.addEventListener('click', (e) =>{
+    e.target.style.color = "#03ADAA";
+    btn_promotion.style.color = "#555555";
+    btn_before_after.style.color = "#555555";
+    btn_comment.style.color = "#555555";
+    about.style.display='block';
+    promotion.style.display='none';
+    before_after.style.display='none';
+    comment.style.display='none';
+})
+btn_promotion.addEventListener('click', (e) =>{
+    e.target.style.color = "#03ADAA";
+    btn_about.style.color = "#555555";
+    btn_before_after.style.color = "#555555";
+    btn_comment.style.color = "#555555";
+    about.style.display='none';
+    promotion.style.display='block';
+    before_after.style.display='none';
+    comment.style.display='none';
+})
+btn_before_after.addEventListener('click', (e) =>{
+    e.target.style.color = "#03ADAA";
+    btn_promotion.style.color = "#555555";
+    btn_about.style.color = "#555555";
+    btn_comment.style.color = "#555555";
+    about.style.display='none';
+    promotion.style.display='none';
+    before_after.style.display='block';
+    comment.style.display='none';
+})
+btn_comment.addEventListener('click', (e) =>{
+    e.target.style.color = "#03ADAA";
+    btn_promotion.style.color = "#555555";
+    btn_before_after.style.color = "#555555";
+    btn_about.style.color = "#555555";
+    about.style.display='none';
+    promotion.style.display='none';
+    before_after.style.display='none';
+    comment.style.display='block';
+})
